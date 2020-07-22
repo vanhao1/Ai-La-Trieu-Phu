f9fc156deb5089a03d6d9686d52e8ec8b905bcba
vanhao1 <quytranzxcvbnm@gmail.com>
2020-07-22 20:12:43 +0700

    Đã sửa lại source code của CaiDat,Main,Player

--- CaiDatActivity.java
+++ CaiDatActivity.java
@@ -1,16 +1,93 @@
-package com.example.ailatrieuphu_n11_project.activity;
+package tdc.edu.vn.ailatrieuphu_n7_project.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
-import com.example.ailatrieuphu_n11_project.R;
+import tdc.edu.vn.ailatrieuphu_n7_project.App;
+import tdc.edu.vn.ailatrieuphu_n7_project.R;
 
-public class CaiDatActivity extends AppCompatActivity {
+public class CaiDatActivity extends Activity implements View.OnClickListener {
+    private ToggleButton togMusic;
+    private ToggleButton togSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_cai_dat );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caidat);
+
+        togMusic = ((ToggleButton)findViewById(R.id.tog_music));
+        togSound = ((ToggleButton)findViewById(R.id.tog_sound));
+        setBgTogMusic(App.getMusicPlayer().getStateMusic());
+        setBgTogSound(App.getMusicPlayer().getStateSound());
+
+        togMusic.setOnClickListener(this);
+        togSound.setOnClickListener(this);
     }
+
+    public void setBgTogMusic(boolean state){
+        if(state){
+            togMusic.setBackgroundResource(R.drawable.toggle_button_on);
+            togMusic.setChecked(true);
+        }else{
+            togMusic.setBackgroundResource(R.drawable.toggle_button_off);
+            togMusic.setChecked(false);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        App.getMusicPlayer().pauseBgMusic();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        App.getMusicPlayer().resumeBgMusic();
+
+    }
+
+    public void setBgTogSound(boolean state){
+        if(state){
+            togSound.setBackgroundResource(R.drawable.toggle_button_on);
+            togSound.setChecked(true);
+        }else{
+            togSound.setBackgroundResource(R.drawable.toggle_button_off);
+            togSound.setChecked(false);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tog_music:
+                if(togMusic.isChecked()){
+                    togMusic.setBackgroundResource(R.drawable.toggle_button_on);
+                    togMusic.setChecked(true);
+                    App.getMusicPlayer().setStateMusic(true);
+                    App.getMusicPlayer().playBgMusic(R.raw.bgmusic);
+                }else{
+                    togMusic.setBackgroundResource(R.drawable.toggle_button_off);
+                    togMusic.setChecked(false);
+                    App.getMusicPlayer().stopBgMusic();
+                }
+                break;
+            case R.id.tog_sound:
+                setBgTogSound(togSound.isChecked());
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        App.getMusicPlayer().setting(togMusic.isChecked(), togSound.isChecked());
+
+    }
+
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,16 +1,50 @@
-package com.example.ailatrieuphu_n11_project.activity;
+package tdc.edu.vn.ailatrieuphu_n7_project.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.ailatrieuphu_n11_project.R;
+import tdc.edu.vn.ailatrieuphu_n7_project.App;
+import tdc.edu.vn.ailatrieuphu_n7_project.R;
+import tdc.edu.vn.ailatrieuphu_n7_project.dialogs.ThongBaoDialog;
+import tdc.edu.vn.ailatrieuphu_n7_project.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.bg_circle_rotate);
+        animation.setDuration(3000);
+        findViewById(R.id.load).startAnimation(animation);
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                .replace(R.id.frame_main, new HomeFragment())
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        final ThongBaoDialog noticeDialog = new ThongBaoDialog(this);
+        noticeDialog.setCancelable(true);
+        noticeDialog.setNotification("Bạn muốn thoát trò chơi ?", "Đồng ý", "Hủy", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.btn_ok) {
+                    App.getMusicPlayer().stopBgMusic();
+                    finish();
+
+                }
+                noticeDialog.dismiss();
+            }
+        });
+        noticeDialog.show();
     }
 }
